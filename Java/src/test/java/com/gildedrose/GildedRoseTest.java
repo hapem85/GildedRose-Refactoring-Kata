8933f65a93c954dfe734eeda1d79aea02062ebb8
hapem85 <hapem85@gmail.com>
2022-07-04 09:38:52 +0700

    Update GildedRoseTest.java
    Update some border unit tests to increase code coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class GildedRoseTest {
 
-	private static final String NORMAL_ITEM = "normal item";
-	public final static String SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
-	public final static String AGED_BRIE_ITEM = "Aged Brie";
-	public final static String BACKSTAGE_PASSES_ITEM = "Backstage passes to a TAFKAL80ETC concert";
-	public final static String CONJURED_ITEM = "Conjured";
+	private static final String NORMAL_ITEM = "+5 Dexterity Vest";
+	private final static String SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
+	private final static String AGED_BRIE_ITEM = "Aged Brie";
+	private final static String BACKSTAGE_PASSES_ITEM = "Backstage passes to a TAFKAL80ETC concert";
+	private final static String CONJURED_ITEM = "Conjured Mana Cake";
 
 	@Test
 	public void foo() {
@@ -23,11 +23,12 @@ public class GildedRoseTest {
 
 	@Test
 	public void normalItemDegradesSellByDay() {
-		GildedRose app = this.createGildedRoseItem(NORMAL_ITEM, 0, 0);
+		GildedRose app = this.createGildedRoseItem(NORMAL_ITEM, 10, 20);
 
 		app.updateQuality();
 
-		assertEquals(-1, itemSellByDayValue(app));
+		assertEquals(9, itemSellByDayValue(app));
+		assertEquals(19, itemQualityValue(app));
 	}
 	
 	@Test
@@ -69,11 +70,12 @@ public class GildedRoseTest {
 
 	@Test
 	public void agedBrieIncreasesQualityOlderItsGet() {
-		GildedRose app = this.createGildedRoseItem(AGED_BRIE_ITEM, 1, 1);
+		GildedRose app = this.createGildedRoseItem(AGED_BRIE_ITEM, 2, 0);
 
 		app.updateQuality();
 
-		assertEquals(2, itemQualityValue(app));
+		assertEquals(1, itemSellByDayValue(app));
+		assertEquals(1, itemQualityValue(app));
 	}
 
 	@Test
@@ -98,16 +100,17 @@ public class GildedRoseTest {
 	public void sulfurasItemQualityIsEighty() {
 		GildedRose app = this.createGildedRoseItem(SULFURAS_ITEM, 1, 80);
 
+		assertEquals(1, itemSellByDayValue(app));
 		assertEquals(80, itemQualityValue(app));
 	}
 
 	@Test
 	public void sulfurasItemNeverAlterValues() {
-		GildedRose app = this.createGildedRoseItem(SULFURAS_ITEM, 2, 80);
+		GildedRose app = this.createGildedRoseItem(SULFURAS_ITEM, 0, 80);
 
 		app.updateQuality();
 
-		assertEquals(2, itemSellByDayValue(app));
+		assertEquals(0, itemSellByDayValue(app));
 		assertEquals(80, itemQualityValue(app));
 	}
 	
@@ -117,6 +120,7 @@ public class GildedRoseTest {
 
 		app.updateQuality();
 
+		assertEquals(-1, itemSellByDayValue(app));
 		assertEquals(80, itemQualityValue(app));
 	}
 	
@@ -157,8 +161,17 @@ public class GildedRoseTest {
 	}
 	
 	@Test
-	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanElevenAndNeverGreaterThanFifty() {
-		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 10, 49);
+	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanElevenAndReachToFifty() {
+		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 9, 49);
+
+		app.updateQuality();
+
+		assertEquals(50, itemQualityValue(app));
+	}
+	
+	@Test
+	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanElevenAndMaxFifty() {
+		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 9, 50);
 
 		app.updateQuality();
 
@@ -202,7 +215,16 @@ public class GildedRoseTest {
 	}
 	
 	@Test
-	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanSixAndNeverGreaterThanFifty() {
+	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanSixAndMaxFifty() {
+		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 4, 50);
+
+		app.updateQuality();
+
+		assertEquals(50, itemQualityValue(app));
+	}
+	
+	@Test
+	public void backstagePassesItemIncreasesQualityWhenSellByDayLessThanSixAndReachToFifty() {
 		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 5, 49);
 
 		app.updateQuality();
@@ -229,7 +251,7 @@ public class GildedRoseTest {
 	}
 
 	@Test
-	public void backstagePassesItemQualityNeverGreaterThanFifty() {
+	public void backstagePassesItemQualityMaxFifty() {
 		GildedRose app = this.createGildedRoseItem(BACKSTAGE_PASSES_ITEM, 5, 50);
 
 		app.updateQuality();
